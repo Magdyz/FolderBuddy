b3786ba026cb5d48811c133fa239c6fabb10153c
Magdyz <79382097+Magdyz@users.noreply.github.com>
2022-03-17 07:45:45 +0000

    cleaned up code and made if more concise

--- folderBuddy.py
+++ folderBuddy.py
@@ -2,56 +2,27 @@ import os
 
 # Folder name that will be inputed everytime as a question
 
-folderName = input("Type folder name: \n")
-
-# remove the # from the code below and add the Folder name if you prefer to have a constant name everytime without questions.
-# If you choose to use the option below comment the first folderName above. You can only use one variable.
-
-# folderName = "CHANGE THIS TEXT BETWEEN BRACKETS WITH FOLDER NAME"
-
-subfolder1 = "CHANGE-SUBFOLDER-NAME1"
-subfolder2 = "CHANGE-SUBFOLDER-NAME2"
-subfolder3 = "CHANGE-SUBFOLDER-NAME3"
-subfolder4 = "CHANGE-SUBFOLDER-NAME4"
-subfolder5 = "CHANGE-SUBFOLDER-NAME5"
-subfolder6 = "CHANGE-SUBFOLDER-NAME6"
-
-# Add more if you want
-
-# subfolder7 = "Images"
-# subfolder6 = "Documents"
+folderName = input("Type folder name: \n")                                # Prompt user to add folder name 
+# folderName = "CHANGE THIS TEXT BETWEEN BRACKETS WITH FOLDER NAME"       # uncomment if you want the same foldername all the time
 
 
-# The main function that includes all sub-folders.
+# change Subfolder names to the desired subfolders you want. You can add or remove just dont remove the " " 
+
+myFolders = ["subfolder1", "subfolder2", "subfolder3", "subfolder4", "subfolder5", "subfolder6"]
+
+
+# The main function 
 # Nothing needs to be changed here 
 
 def createProject():
 	os.mkdir(folderName)
 	print( "++ " + folderName + ' is created!')
-	os.mkdir(f"{folderName}/{subfolder1}")
-	print( "++ " + folderName + "/" + subfolder1 + ' is created!')   
-	os.mkdir(f"{folderName}/{subfolder2}")
-	print( "++ " + folderName + "/" + subfolder2 + ' is created!')   
-	os.mkdir(f"{folderName}/{subfolder3}")
-	print( "++ " + folderName + "/" + subfolder3 + ' is created!')   
-	os.mkdir(f"{folderName}/{subfolder4}")
-	print( "++ " + folderName + "/" + subfolder4 + ' is created!')   
-	os.mkdir(f"{folderName}/{subfolder5}")
-	print( "++ " + folderName + "/" + subfolder5 + ' is created!')   	
-	os.mkdir(f"{folderName}/{subfolder6}")
-	print( "++ " + folderName + "/" + subfolder6 + ' is created!')   
-	
-## To add more subfolders 
-## Add subfolder7 = "CHANGE-SUBFOLDER-NAME7" like the example above
-## then
-## remove "#" and copy and paste the code below with the same spaces and change subfolder#number accordingly
 
-#	os.mkdir(f"{folderName}/{subfolder7}")
-#	print( "++ " + folderName + "/" + subfolder7 + ' is created!')   
-#	os.mkdir(f"{folderName}/{subfolder8}")
-#	print( "++ " + folderName + "/" + subfolder8 + ' is created!')   
+	for item in myFolders:
+		os.mkdir(f"{folderName}/{item}")
+		print( "++ " + folderName + "/" + item + ' is created!')   
 
 
 createProject()
-print("Directory is created")
+print("Mission Accomplished")
 
