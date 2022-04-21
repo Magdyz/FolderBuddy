106c926a9178e2166ee265e1071f953a7b540b4c
Magdyz <79382097+Magdyz@users.noreply.github.com>
2022-04-21 10:01:55 +0100

    foldername as an argument option

--- folderBuddy.py
+++ folderBuddy.py
@@ -1,18 +1,26 @@
 import os
 
-# Folder name that will be inputed everytime as a question
+##### folder name that will be inputed everytime as a question
 
 folderName = input("Type folder name: \n")                                # Prompt user to add folder name 
+
+##### fixed folder name
+
 # folderName = "CHANGE THIS TEXT BETWEEN BRACKETS WITH FOLDER NAME"       # uncomment if you want the same foldername all the time
 
+##### folder name as an argument
 
-# change Subfolder names to the desired subfolders you want. You can add or remove just don't remove the " " 
+#import sys                                    # uncomment if you want the foldername to be inputed as an argument 
+#folderName = " ".join(sys.argv[1:])           # uncomment if you want the foldername to be inputed as an argument 
+
+
+##### change Subfolder names to the desired subfolders you want. You can add or remove just don't remove the " " 
 
 myFolders = ["subfolder1", "subfolder2", "subfolder3", "subfolder4", "subfolder5", "subfolder6"]
 
 
-# The main function 
-# Nothing needs to be changed here 
+##### The main function 
+##### Nothing needs to be changed here 
 
 def createProject():
 	os.mkdir(folderName)
